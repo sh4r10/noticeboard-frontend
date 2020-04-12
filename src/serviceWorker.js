0575b3ff37ce40c8eef3523bbc5814989ed2a10c
Shariq Shahbaz <iamsh4r10@gmail.com>
2020-04-12 19:49:11 +0200

    service worker 7

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,21 +1,22 @@
-// self.addEventListener('push', e =>{
-//     console.log("push received", e);
-//     const data = e.data.json();
-//     console.log(data);
-//     self.registration.showNotification("Work in progress...", {
-//         body: "Notified by sh4r10",
-//         icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png"
-//     });
-//     console.log("Notification sent");
-// })
-
-self.addEventListener('push', function(event) {
-    navigator.serviceWorker.ready.then(function(registration) {
-        registration.showNotification('Vibration Sample', {
-          body: 'Buzz! Buzz!',
-          icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png',
-          vibrate: [200, 100, 200, 100, 200, 100, 200],
-          tag: 'vibration-sample'
-        });
+self.addEventListener('push', e =>{
+    console.log("push received", e);
+    const data = e.data.json();
+    console.log(data);
+    self.registration.showNotification("Work in progress...", {
+        body: "Notified by sh4r10",
+        icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png"
     });
-  });
+    console.log("Notification sent");
+})
+
+// self.addEventListener('push', function(event) {
+//     console.log("push recieved");
+//     navigator.serviceWorker.ready.then(function(registration) {
+//         registration.showNotification('Vibration Sample', {
+//           body: 'Buzz! Buzz!',
+//           icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png',
+//           vibrate: [200, 100, 200, 100, 200, 100, 200],
+//           tag: 'vibration-sample'
+//         });
+//     });
+//   });
